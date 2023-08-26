5f8697e736fefbb3dd404e6cb09a794fd64da35e
bhugo <41992011+bhugoVilela@users.noreply.github.com>
2023-08-26 01:40:08 +0100

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -179,7 +179,9 @@ setup_neovim() {
 
 install_xcode_cmd_line_tools() {
 	_echo "installing xcode command line tools..."
+	set +e
 	xcode-select --install
+	set -e
 	if [ $? -ne 0 ]; then
 		_echo "already installed, skipping..."
 	else
